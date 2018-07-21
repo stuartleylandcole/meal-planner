13cf7e9d55fdf1734c9078debe98369bf4d86f3a
Stuart Leyland <stuartleyland@gmail.com>
2018-07-21 15:07:20 +0100

    Add AWS Amplify to connect to backend

---
+++ meal-planner-client/src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://nekzgqwegj.execute-api.eu-west-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_2OIPrqwS9",
+    APP_CLIENT_ID: "1gg5ojrqgfrcremuriocqdjhdc",
+    IDENTITY_POOL_ID: "eu-west-2:0f57ec6b-2627-4b82-be5d-1b48b89fed55"
+  }
+};
