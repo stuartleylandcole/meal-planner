50fa7755b42b8f8d4e0311a4356672db19d9a4a2
Stuart Leyland-Cole <stuartleyland@gmail.com>
2018-07-31 16:48:51 +0100

    Add components that ensures users are authenticated and redirect if not/when they are

--- meal-planner-client/src/Routes.js
+++ meal-planner-client/src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
@@ -11,9 +13,9 @@ import Meals from "./containers/Meals";
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute path="/meals/new" exact component={NewMeal} props={childProps} />
-    <AppliedRoute path="/meals/:id" exact component={Meals} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/meals/new" exact component={NewMeal} props={childProps} />
+    <AuthenticatedRoute path="/meals/:id" exact component={Meals} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
